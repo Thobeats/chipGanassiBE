e11c0d20213619e51c199a81d60caceb94ab4213
Thobeats <tobiy23@gmail.com>
2022-04-29 12:16:02 +0100

    update

--- newspaper.sql
+++ newspaper.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `heroku_8ec114e233ac83e`
+-- Database: `newspaper`
 --
 
 -- --------------------------------------------------------
