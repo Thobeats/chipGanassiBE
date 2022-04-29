9d7266a86e17f47013b2c1109a2a3efc4cb487a0
Thobeats <tobiy23@gmail.com>
2022-04-29 13:08:22 +0100

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 //$route['default_controller'] = 'admin';
-$route['default_controller'] = 'admin/races';
+$route['default_controller'] = 'admin/index';
 $route['404_override'] = 'home/error';
 $route['translate_uri_dashes'] = FALSE;
